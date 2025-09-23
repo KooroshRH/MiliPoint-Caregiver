03772c6ad4df41a3b5a68f9efbe355b90ebc8dc6
Koorosh Roohi <korosh.roohi9731@gmail.com>
2025-09-23 14:28:45 -0400

    Add zone-based feature modulation to PointNet: implement ZoneFiLM and integrate with PointNet architecture.

---
+++ mmrnet/models/pointnet-film.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+from pointnet import PointNet
+
+class ZoneFiLM(nn.Module):
+    def __init__(self, feat_dim, zone_dim=6, embed_dim=16):
+        super().__init__()
+        self.zone_embed = nn.Linear(zone_dim, embed_dim)   # embed zone one-hot
+        self.gamma_gen = nn.Linear(embed_dim, feat_dim)    # generate scale
+        self.beta_gen  = nn.Linear(embed_dim, feat_dim)    # generate shift
+
+    def forward(self, f, zone_onehot):
+        """
+        f: (B*N, feat_dim)  - per-point features
+        zone_onehot: (B*N, zone_dim) - one-hot zone
+        """
+        e = self.zone_embed(zone_onehot)       # (B*N, embed_dim)
+        gamma = self.gamma_gen(e)              # (B*N, feat_dim)
+        beta = self.beta_gen(e)                # (B*N, feat_dim)
+        return gamma * f + beta
+
+class PointNetWithFiLM(PointNet):
+    def __init__(self, info, zone_dim=6):
+        super().__init__(info)
+        self.zone_film = ZoneFiLM(feat_dim=128, zone_dim=zone_dim)  
+        # 128 = output dim of sa1_module in your code
+
+    def forward(self, data, zone_onehot):
+        # data: (B, N, 3)
+        # zone_onehot: (B, N, zone_dim)
+
+        batchsize, npoints, _ = data.shape
+        x = data.reshape((batchsize * npoints, 3))
+        zone_flat = zone_onehot.reshape((batchsize * npoints, -1))  # flatten
+
+        batch = torch.arange(batchsize).repeat_interleave(npoints).to(x.device)
+        sa0_out = (x, x, batch)
+
+        # SA1
+        x, pos, batch = self.sa1_module(*sa0_out)
+
+        # Apply FiLM on per-point features
+        x = self.zone_film(x, zone_flat)
+
+        # SA2 + SA3 as usual
+        sa2_out = self.sa2_module(x, pos, batch)
+        sa3_out = self.sa3_module(*sa2_out)
+        x, pos, batch = sa3_out
+
+        # Classifier head
+        if self.num_classes is None:
+            y = [self.mlp[f'branch_{i}'](x) for i in range(self.num_points)]
+            y = torch.stack(y, dim=1)
+        else:
+            y = self.mlp(x)
+        return y
