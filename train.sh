55b8bd37bcaf810f3b148f4c4bec0479cd6439c3
Koorosh Roohi <t125959uhn@h4huhnlogin2.h4h.uhnresearch.ca>
2025-09-25 11:45:18 -0400

    New zoned training and model method.

--- train.sh
+++ train.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 #SBATCH -A kite_gpu
-#SBATCH -t 3-0:0:0
-#SBATCH --output out_hybrid_carelab_act_test.out
+#SBATCH -t 0-1:0:0
+#SBATCH --output out_pointnet_zoned_carelab_act_test.out
 #SBATCH -p gpu
 #SBATCH --gres=gpu:v100:1
-#SBATCH -C gpu32g
 #SBATCH --nodes 1
 #SBATCH --mem 32G
 #SBATCH -c 4
@@ -23,4 +22,4 @@ module load python3/3.10.9
 
 source milenv/bin/activate
 
-python mm.py train mmr_act hybrid --save-name hybrid_stack40_act -config ./configs/action/mmr_action_stack_40_point_carelab_5fold_0.toml -a gpu -m 400 -lr 1e-3 -b 64
+python mm.py train mmr_act pointnet-film --save-name pointnet-film_stack40_act -config ./configs/action/mmr_action_stack_40_point_carelab_5fold_0.toml -a gpu -m 400 -lr 1e-3 -b 64
