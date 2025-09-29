cf41cc30443732395f5123a67c154e124d193e56
Koorosh Roohi <korosh.roohi9731@gmail.com>
2025-09-29 14:55:03 -0400

    Add subject_id to mmr_dataset_config for enhanced dataset handling

--- mmrnet/cli.py
+++ mmrnet/cli.py
@@ -219,6 +219,7 @@ class Main:
         }
         mmr_dataset_config = {
             'raw_data_path': a.dataset_raw_data_path,
+            'subject_id': a.dataset_subject_id,
             'seed': a.dataset_seed,
             'train_split': a.dataset_train_split,
             'val_split': a.dataset_val_split,
