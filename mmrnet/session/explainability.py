baf8a8404b87450c5d29ba5104738d63e3c70c02
Koorosh Roohi <t125959uhn@h4huhnlogin2.h4h.uhnresearch.ca>
2026-01-26 19:52:20 -0500

    Add handle for dgcnn explainability

--- mmrnet/session/explainability.py
+++ mmrnet/session/explainability.py
@@ -380,8 +380,8 @@ def visualize_point_saliency_3d(points, saliency, title="Point Saliency", save_p
     pts = pts[sort_idx]
     sal = sal[sort_idx]
 
-    pts[:, 0] = pts[:, 0] + 2.4
-    pts[:, 1] = pts[:, 1] + 1.45
+    #pts[:, 0] = pts[:, 0] + 2.45
+    #pts[:, 1] = pts[:, 1] + 1.45
 
     # Create colormap
     norm = Normalize(vmin=sal.min(), vmax=sal.max())
@@ -450,8 +450,8 @@ def visualize_point_saliency_2d(points, saliency, title="Point Saliency", save_p
     pts = pts[sort_idx]
     sal = sal[sort_idx]
 
-    pts[:, 0] = pts[:, 0] + 2.4
-    pts[:, 1] = pts[:, 1] + 1.45
+    #pts[:, 0] = pts[:, 0] + 2.45
+    #pts[:, 1] = pts[:, 1] + 1.45
 
     projections = [
         (pts[:, 0], pts[:, 1], 'X (m)', 'Y (m)', 'XY Projection (Top View)'),
